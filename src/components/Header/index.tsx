05b7e6da2b1dc825ca484484e4186e485727f984
JANIO <janio.carvalho@pixter.com.br>
2021-12-09 21:29:29 -0300

    UPDATE: remove sharing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -34,7 +34,7 @@ export function Header({ title, action }: Props) {
 
       <Text style={styles.title}>{title}</Text>
 
-      {action ? <View>{action}</View> : <View style={{ width: 24 }} />}
+      {/* {action ? <View>{action}</View> : <View style={{ width: 24 }} />} */}
     </LinearGradient>
   );
 }
